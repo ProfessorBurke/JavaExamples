6fbe9e0baca46453e30b16cb5d8513a0c73ea3a9
Maggie Burke <msb@alumni.brown.edu>
2022-05-16 10:33:05 -0400

    Add files via upload

---
+++ AddClassFileEclipse/SimpleClassExample/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Rectangle","l":"getArea()"},{"p":"<Unnamed>","c":"Rectangle","l":"getPerimeter()"},{"p":"<Unnamed>","c":"Rectangle","l":"Rectangle(double, double)","u":"%3Cinit%3E(double,double)"}];updateSearchResults();
