7204558137c349ea7a50b7a1b66c51409cfdb74c
Maggie Burke <msb@alumni.brown.edu>
2024-02-24 11:46:21 -0500

    Add files via upload

---
+++ IntroJava/Lesson2/MoonWeightCalculator.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/**
+ * Obtain's the user's weight on Earth, then calculates and displays the user's
+ * weight on the moon using the formula earthWeight / 9.81 * 1.622.
+ * 2/23/2024
+ * 
+ * @author Maggie
+ *
+ */
+public class MoonWeightCalculator {
+
+	public static void main(String[] args) {
+		
+		final double EARTH_GRAVITY = 9.81;
+		final double MOON_GRAVITY = 1.622;
+		
+		Scanner stdin;       // Scanner for obtaining input from the console.
+		double earthWeight;  // User's weight on Earth.
+		double moonWeight;   // User's calculated weight on the moon.
+		
+		// Obtains the weight on Earth from the user.
+		stdin = new Scanner(System.in);
+		System.out.print("Please enter your weight on Earth: ");
+		earthWeight = stdin.nextDouble();
+		stdin.close();
+		
+		// Calculate the user's weight on the moon.
+		moonWeight = earthWeight / EARTH_GRAVITY * MOON_GRAVITY;
+		
+		// Display the user's weight on the moon.
+		System.out.println("Your weight on the moon is " + moonWeight + ".");
+	}
+
+}
