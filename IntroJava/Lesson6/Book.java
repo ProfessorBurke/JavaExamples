aee373e5bbe2eeb006f8a13a7f38bd3744309464
Maggie Burke <msb@alumni.brown.edu>
2024-10-31 16:54:45 -0400

    Update Book.java
    Added Javadoc and prepended some instance variables with this. (that weren't previously prepended).

--- IntroJava/Lesson6/Book.java
+++ IntroJava/Lesson6/Book.java
@@ -5,27 +5,44 @@
  * 
  * @author Maggie
  *
- */
-public class Book {
+ */public class Book {
 	
+	/**
+	 * The content of the book as plain text. 
+	 */
 	private String text;
+	/**
+	 * The user's current location in the book.
+	 */
 	private int bookmark;
 	
+	/**
+	 * Construct the book from the text parameter, starting bookmark at zero.
+	 * 
+	 * @param text	The plain text of the book.
+	 */
 	public Book(String text) {
 		this.text = text;
 		this.bookmark = 0;
 	}
 	
+	/**
+	 * Return the next page (100 characters) of the book and advance the
+	 * bookmark field to the next page.
+	 * 
+	 * @return	The 100 characters from bookmark to bookmark+100, or
+	 * 			the last length-bookmark characters.
+	 */
 	public String nextPage() {
 		// Do not allow the end index to extend beyond the end of the text.
-		int endIndex = this.text.length() < bookmark + 100 ? 
-				this.text.length() - 1 : bookmark + 100;
+		int endIndex = this.text.length() < this.bookmark + 100 ? 
+				this.text.length() - 1 : this.bookmark + 100;
 		// Create a "page" of 100 characters.
-		String page = this.text.substring(bookmark, endIndex);
+		String page = this.text.substring(this.bookmark, endIndex);
 		// Move the bookmark along.
-		bookmark += 100;
-		if (bookmark >= this.text.length()) {
-			bookmark = this.text.length() - 1;
+		this.bookmark += 100;
+		if (this.bookmark >= this.text.length()) {
+			this.bookmark = this.text.length() - 1;
 		}
 		return page; 
 	}
